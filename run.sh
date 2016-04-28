2462f5718db76ff1647a728ccd1aeb11330653dd
Mikhail Osipov <osipov@Mikhails-MacBook-Pro.local>
2016-04-28 22:01:40 +0700

    imporved run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,11 @@
 case $1 in
 start)
     mkdir -p /home/forge/rfid_data
-    ./small_tcp_server &
+    ./small_tcp_server &>/dev/null &
+    echo $! > /tmp/small_tcp_server.pid
+;;
+stop)
+    pkill small_tcp_server
 ;;
 build)
     go build small_tcp_server.go
